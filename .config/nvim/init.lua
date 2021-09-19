c309f3ca14f892afb578a29e9c42910ece6c1ed5
Volodymyr Chernetskyi <19735328+chernetskyi@users.noreply.github.com>
2021-09-19 17:36:30 +0300

    Rewrite Neovim config to Lua from vimscript

---
+++ .config/nvim/init.lua
@@ -0,0 +1,123 @@
+require 'plugins'
+
+-- Colors {{{
+vim.cmd[[colorscheme nord]]
+vim.cmd[[syntax enable]]
+vim.opt.termguicolors = true
+-- }}}
+
+-- UI {{{
+vim.opt.cursorline = true
+vim.opt.lazyredraw = true
+vim.opt.showmode = false
+vim.opt.wrap = false
+
+vim.opt.number = true
+vim.opt.numberwidth = 1
+vim.cmd[[
+augroup toggle-numbers
+  autocmd!
+  autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
+  autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
+augroup END
+]]
+
+vim.opt.list = true
+vim.opt.listchars = 'tab:-->,space:·'
+
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 4
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wildmenu = true
+vim.opt.wildmode = 'list:longest'
+-- }}}
+
+-- Indentation {{{
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+-- }}}
+
+-- Folding {{{
+vim.opt.foldlevelstart = 99
+vim.opt.foldmethod = 'indent'
+-- }}}
+
+-- Search {{{
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.inccommand = 'nosplit'
+vim.cmd[[
+augroup vimrc-incsearch-highlight
+    autocmd!
+    autocmd CmdlineEnter /,\? :set hlsearch
+    autocmd CmdlineLeave /,\? :set nohlsearch
+augroup END
+]]
+-- }}}
+
+-- History {{{
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.writebackup = false
+-- }}}
+
+-- Movement {{{
+vim.api.nvim_set_keymap('n', 'Y', 'y$', {noremap = true})
+
+vim.api.nvim_set_keymap('n', 'n', 'nzzzv', {noremap = true})
+vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', {noremap = true})
+vim.api.nvim_set_keymap('n', 'J', 'mzJ`z', {noremap = true})
+
+vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', {noremap = true})
+-- }}}
+
+-- Undo breakpoints {{{
+vim.api.nvim_set_keymap('i', '.', '.<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', ',', ',<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', '!', '!<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', '?', '?<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', '{', '{<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', '}', '}<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', '[', '[<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', ']', ']<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', '(', '(<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', ')', ')<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', '<', '<<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', '>', '><c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', '=', '=<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', ':', ':<c-g>u', {noremap = true})
+vim.api.nvim_set_keymap('i', ';', ';<c-g>u', {noremap = true})
+-- }}}
+
+-- Misc {{{
+vim.cmd[[filetype plugin indent on]]
+vim.opt.hidden = true
+vim.opt.modelines = 1
+
+vim.cmd[[
+augroup map-capslock-to-esc
+    autocmd VimEnter * silent! !xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
+    autocmd VimLeave * silent! !xmodmap -e 'clear Lock' -e 'keycode 0x42 = Caps_Lock'
+augroup END
+]]
+
+vim.cmd[[
+augroup lua-highlight
+    autocmd!
+    autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank()
+augroup END
+]]
+-- }}}
+
+-- vim:foldmethod=marker:foldlevel=0
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,97 @@
+return require('packer').startup(function()
+  use {'wbthomason/packer.nvim',
+  -- config {{{
+        config = function()
+          vim.cmd[[
+            augroup recompile-packer-on-save
+              autocmd!
+              autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+            augroup end
+          ]]
+        end}
+  -- }}}
+
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-repeat'
+  use {'andymass/vim-matchup', event = 'VimEnter'}
+
+  use 'neovim/nvim-lspconfig'
+  use {'hrsh7th/nvim-cmp', requires = {'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer'},
+  -- config {{{
+        config = function()
+          local cmp = require 'cmp'
+          cmp.setup({
+            mapping = {
+              ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<C-e>'] = cmp.mapping.close(),
+              ['<CR>'] = cmp.mapping.confirm(),
+              ['<Tab>'] = cmp.mapping.select_next_item()
+            },
+            sources = {
+              { name = 'nvim_lsp' },
+              { name = 'buffer' }
+            }
+          })
+
+          vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+        end}
+  -- }}}
+  use {'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons',
+  -- config {{{
+        config = function()
+          vim.g.nvim_tree_ignore = {'.git'}
+          vim.g.nvim_tree_auto_close = 1
+          vim.g.nvim_tree_tab_open = 1
+          vim.g.nvim_tree_add_trailing = 1
+          vim.g.nvim_tree_hijack_cursor = 0
+
+          vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true})
+        end}
+  -- }}}
+
+  use {'ionide/Ionide-vim', requires = {'neovim/nvim-lspconfig', 'hrsh7th/nvim-cmp'}, run = 'make fsautocomplete',
+  -- config {{{
+        config = function()
+          require('lspconfig')['fsautocomplete'].setup {
+            capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+            }
+
+            vim.cmd[[
+              augroup fsharp-tooltip
+                autocmd!
+                autocmd CursorHold *.fs,*.fsi,*.fsx call fsharp#showTooltip()
+              augroup END
+            ]]
+
+        end}
+  -- }}}
+  use 'hashivim/vim-terraform'
+  use 'lervag/vimtex'
+
+  use {'shaunsingh/nord.nvim',
+  -- config {{{
+        config = function()
+          vim.g.nord_contrast = true
+          vim.g.nord_borders = true
+          vim.g.nord_italic = true
+          require('nord').set()
+        end}
+  -- }}}
+  use {'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons'},
+  -- config {{{
+        config = function()
+          require('lualine').setup {
+            options = {theme = 'nord'},
+            sections = {lualine_x = {'filetype'}},
+            tabline = {lualine_a = {'filename'}},
+            extensions = {'fugitive', 'nvim-tree'}
+          }
+        end}
+  -- }}}
+end)
+
+-- vim:foldmethod=marker:foldlevel=0
