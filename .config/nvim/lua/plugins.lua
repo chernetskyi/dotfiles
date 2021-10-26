08c90cbb72e227bb7e911420fd0cc92978eea8b2
Volodymyr Chernetskyi <19735328+chernetskyi@users.noreply.github.com>
2021-10-26 19:28:55 +0300

    Update plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -15,9 +15,24 @@ return require('packer').startup(function()
   use 'tpope/vim-fugitive'
   use 'tpope/vim-surround'
   use 'tpope/vim-repeat'
-  use {'andymass/vim-matchup', event = 'VimEnter'}
 
   use 'neovim/nvim-lspconfig'
+
+  use {'nvim-treesitter/nvim-treesitter', branch = '0.5-compat', run = ':TSUpdate',
+  -- config {{{
+        config = function()
+          require('nvim-treesitter.configs').setup{highlight = {enable = true}}
+        end}
+  -- }}}
+
+  use {'andymass/vim-matchup',
+  -- config {{{
+        config = function()
+          vim.g.loaded_matchit = 1
+          require('nvim-treesitter.configs').setup{matchup = {enable = true}}
+        end}
+  -- }}}
+
   use {'hrsh7th/nvim-cmp', requires = {'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer'},
   -- config {{{
         config = function()
@@ -40,23 +55,13 @@ return require('packer').startup(function()
           vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
         end}
   -- }}}
-  use {'nvim-treesitter/nvim-treesitter', branch = '0.5-compat', run = ':TSUpdate',
-  -- config {{{
-        config = function()
-          require('nvim-treesitter.configs').setup {
-            highlight = {enable = true}
-          }
-        end}
-  -- }}}
+
   use {'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons',
   -- config {{{
         config = function()
           vim.g.nvim_tree_ignore = {'.git'}
-          vim.g.nvim_tree_auto_close = 1
-          vim.g.nvim_tree_tab_open = 1
           vim.g.nvim_tree_add_trailing = 1
-          vim.g.nvim_tree_hijack_cursor = 0
-
+          require('nvim-tree').setup{auto_close = true}
           vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true})
         end}
   -- }}}
@@ -66,18 +71,18 @@ return require('packer').startup(function()
         config = function()
           require('lspconfig')['fsautocomplete'].setup {
             capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-            }
-
-            vim.cmd[[
-              augroup fsharp-tooltip
-                autocmd!
-                autocmd CursorHold *.fs,*.fsi,*.fsx call fsharp#showTooltip()
-              augroup END
-            ]]
-
+          }
+          vim.cmd[[
+            augroup fsharp-tooltip
+              autocmd!
+              autocmd CursorHold *.fs,*.fsi,*.fsx call fsharp#showTooltip()
+            augroup END
+          ]]
         end}
   -- }}}
+
   use 'hashivim/vim-terraform'
+
   use 'lervag/vimtex'
 
   use {'shaunsingh/nord.nvim',
@@ -85,21 +90,35 @@ return require('packer').startup(function()
         config = function()
           vim.g.nord_contrast = true
           vim.g.nord_borders = true
-          vim.g.nord_italic = true
+          vim.g.nord_italic = false
           require('nord').set()
         end}
   -- }}}
-  use {'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons'},
+
+  use {'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons'},
   -- config {{{
         config = function()
           require('lualine').setup {
             options = {theme = 'nord'},
             sections = {lualine_x = {'filetype'}},
-            tabline = {lualine_a = {'filename'}},
+            tabline = {lualine_a = {'buffers'}, lualine_z = {'tabs'}},
             extensions = {'fugitive', 'nvim-tree'}
           }
         end}
+  --  }}}
+
+  use {'aserowy/tmux.nvim',
+  -- config {{{
+    config = function()
+      require("tmux").setup{
+        navigation = {enable_default_keybindings = true},
+        resize = {enable_default_keybindings = true}
+      }
+    end}
   -- }}}
+
+  use 'dstein64/vim-startuptime'
+
 end)
 
 -- vim:foldmethod=marker:foldlevel=0
