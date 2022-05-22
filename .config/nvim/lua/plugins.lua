c6fb3912d51997e248db2fefa6a492ed74f8dd80
Volodymyr Chernetskyi <19735328+chernetskyi@users.noreply.github.com>
2022-05-22 19:54:27 +0300

    Remove unused snippet plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -14,7 +14,7 @@ return require('packer').startup(function()
         end}
   -- }}}
 
-  use {'hrsh7th/nvim-cmp', requires = {'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-vsnip', 'L3MON4D3/LuaSnip', 'hrsh7th/cmp-cmdline', 'petertriho/cmp-git'},
+  use {'hrsh7th/nvim-cmp', requires = {'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline', 'petertriho/cmp-git'},
   -- config {{{
         config = function()
           vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
@@ -49,18 +49,12 @@ return require('packer').startup(function()
 
           local cmp = require 'cmp'
           cmp.setup({
-            snippet = {
-              expand = function(args)
-                require('luasnip').lsp_expand(args.body)
-              end
-            },
             mapping = {
               ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
               ['<CR>'] = cmp.mapping.confirm({behavior = cmp.ConfirmBehavior.Replace})
             },
             sources = cmp.config.sources({
               { name = 'nvim_lsp' },
-              { name = 'luasnip' },
             }, {
               { name = 'buffer' },
             }),
