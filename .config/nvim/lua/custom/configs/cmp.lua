698654d56ae627693de9ca059977a0dc49009918
Volodymyr Chernetskyi <19735328+chernetskyi@users.noreply.github.com>
2023-06-24 14:32:49 +0300

    Fix completion without selection

--- .config/nvim/lua/custom/configs/cmp.lua
+++ .config/nvim/lua/custom/configs/cmp.lua
@@ -1,5 +1,14 @@
+local cmp = require "cmp"
+
 return {
   completion = {
     completeopt = "menuone,noselect",
+  },
+
+  mapping = {
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = false,
+    },
   },
 }
