0b7c638ba277091a9afd55bd6d06670cba209f27
Volodymyr Chernetskyi <19735328+chernetskyi@users.noreply.github.com>
2023-09-08 15:26:02 +0200

    Add missing language servers

--- .config/nvim/lua/custom/configs/mason.lua
+++ .config/nvim/lua/custom/configs/mason.lua
@@ -1,9 +1,11 @@
 return {
   ensure_installed = {
     "ansible-language-server",
+    "bash-language-server",
     "css-lsp",
     "docker-compose-language-service",
     "dockerfile-language-server",
+    "groovy-language-server",
     "helm-ls",
     "html-lsp",
     "json-lsp",
