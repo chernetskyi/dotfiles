e1ddfaf371b565439bfee37291c072058e7f7ee3
Volodymyr Chernetskyi <19735328+chernetskyi@users.noreply.github.com>
2024-05-21 17:20:23 +0200

    Add oil.nvim plugin

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,12 @@
+return {
+  'stevearc/oil.nvim',
+  cmd = 'Oil',
+  keys = {
+    { '<leader>n', function() require('oil').open() end, desc = 'Open parent directory' },
+  },
+  opts = {
+    columns = {},
+    skip_confirm_for_simple_edits = true,
+    constrain_cursor = "name",
+  },
+}
