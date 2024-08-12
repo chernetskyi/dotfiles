b057e591dfa2aaa37547eba94f0e69a724b199b9
Volodymyr Chernetskyi <19735328+chernetskyi@users.noreply.github.com>
2024-08-12 22:31:25 +0200

    Remove unused plugin

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,15 +1,7 @@
 return {
   'numToStr/Comment.nvim',
   event = 'BufRead',
-  dependencies = {
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  opts = {
+    ignore = '^$',
   },
-  config = function()
-    require('ts_context_commentstring').setup({ enable_autocmd = false })
-    require('Comment').setup({
-      ignore = '^$',
-      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-    })
-  end,
 }
