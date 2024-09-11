1b288b141373f398de5562fd670d783ff81e85c2
Volodymyr Chernetskyi <19735328+chernetskyi@users.noreply.github.com>
2024-09-11 16:26:27 +0200

    Set up language servers without Mason

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -11,68 +11,65 @@ return {
     'hrsh7th/cmp-nvim-lsp',
   },
   config = function()
+    local mason_managed_servers = {
+      'bashls',
+      'cssls',
+      'docker_compose_language_service',
+      'dockerls',
+      'groovyls',
+      'helm_ls',
+      'html',
+      'jsonls',
+      'lua_ls',
+      'marksman',
+      'terraformls',
+      'yamlls',
+    }
+
     require('mason').setup()
+    require('mason-lspconfig').setup({ ensure_installed = mason_managed_servers })
 
-    require('mason-lspconfig').setup({
-      ensure_installed = {
-        'basedpyright',
-        'bashls',
-        'cssls',
-        'docker_compose_language_service',
-        'dockerls',
-        'groovyls',
-        'helm_ls',
-        'html',
-        'jsonls',
-        'lua_ls',
-        'marksman',
-        'ruff',
-        'terraformls',
-        'tflint',
-        'yamlls',
+    local servers = vim.list_extend(mason_managed_servers, {
+      'basedpyright',
+      'ruff',
+      'tflint',
+    })
+
+    local lspconfig = require('lspconfig')
+    local default_capabilities = require('cmp_nvim_lsp').default_capabilities()
+    for _, server in ipairs(servers) do
+      lspconfig[server].setup({ capabilities = default_capabilities })
+    end
+
+    lspconfig['basedpyright'].setup({
+      capabilities = default_capabilities,
+      settings = {
+        basedpyright = {
+          disableOrganizeImports = true,
+          analysis = {
+            ignore = { '*' },
+          },
+        },
       },
-      handlers = {
-        function(server_name)
-          require('lspconfig')[server_name].setup({
-            capabilities = require('cmp_nvim_lsp').default_capabilities(),
-          })
-        end,
+    })
 
-        basedpyright = function()
-          require('lspconfig').basedpyright.setup({
-            capabilities = require('cmp_nvim_lsp').default_capabilities(),
-            settings = {
-              basedpyright = {
-                disableOrganizeImports = true,
-                analysis = {
-                  ignore = { '*' },
-                },
-              },
-            },
-          })
-        end,
-
-        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
-        ['lua_ls'] = function()
-          require('lspconfig').lua_ls.setup({
-            capabilities = require('cmp_nvim_lsp').default_capabilities(),
-            on_init = function(client)
-              local path = client.workspace_folders[1].name
-              if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-                return
-              end
-              client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                runtime = { version = 'LuaJIT' },
-                workspace = {
-                  checkThirdParty = false,
-                  library = { vim.env.VIMRUNTIME },
-                },
-              })
-            end,
-            settings = { Lua = {} },
-          })
-        end,
-      },
+    -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+    lspconfig['lua_ls'].setup({
+      capabilities = default_capabilities,
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+          return
+        end
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          runtime = { version = 'LuaJIT' },
+          workspace = {
+            checkThirdParty = false,
+            library = { vim.env.VIMRUNTIME },
+          },
+        })
+      end,
+      settings = { Lua = {} },
     })
   end,
 }
