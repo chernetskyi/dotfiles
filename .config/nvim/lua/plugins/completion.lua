65078cbc70370413e12a30f0a54e5a78c4aeeabc
Volodymyr Chernetskyi <19735328+chernetskyi@users.noreply.github.com>
2024-12-18 21:15:59 +0100

    Replace nvim-cmp with blink.cmp

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,77 +1,21 @@
 return {
   {
-    'hrsh7th/nvim-cmp',
+    'saghen/blink.cmp',
     event = 'InsertEnter',
     dependencies = {
-      {
-        'L3MON4D3/LuaSnip',
-        build = 'make install_jsregexp',
-      },
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-cmdline',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-      'saadparwaiz1/cmp_luasnip',
+      'rafamadriz/friendly-snippets',
     },
-    config = function()
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
-      local types = require('cmp.types')
-
-      luasnip.config.setup()
-
-      cmp.setup({
-        mapping = {
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-        },
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-          { name = 'buffer' },
-        },
-        confirmation = { default_behavior = types.cmp.ConfirmBehavior.Replace },
-      })
-
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' },
-        },
-        formatting = { fields = { 'abbr', 'menu' } },
-      })
-
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        }),
-        formatting = { fields = { 'abbr', 'menu' } },
-        matching = { disallow_symbol_nonprefix_matching = false },
-      })
-    end,
+    version = 'v0.*',
+    opts = {
+      appearance = { use_nvim_cmp_as_default = true, },
+      completion = {
+        documentation = { auto_show = true },
+      },
+      signature = { enabled = true },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+    },
+    opts_extend = { 'sources.default' },
   },
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,67 +8,68 @@ return {
         cmd = 'Mason',
         config = true,
       },
-      'hrsh7th/cmp-nvim-lsp',
+      'saghen/blink.cmp',
     },
-    config = function()
-      local mason_servers = {
-        'bashls',
-        'cssls',
-        'docker_compose_language_service',
-        'dockerls',
-        'groovyls',
-        'helm_ls',
-        'html',
-        'jsonls',
-        'lua_ls',
-        'marksman',
-        'terraformls',
-        'yamlls',
-      }
-      local global_servers = {
-        'basedpyright',
-        'ruff',
-        'tflint',
-      }
+    opts = {
+      servers = {
+        -- mason
+        bashls = {},
+        cssls = {},
+        docker_compose_language_service = {},
+        dockerls = {},
+        groovyls = {},
+        helm_ls = {},
+        html = {},
+        jsonls = {},
+        lua_ls = {
+          -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
+          on_init = function(client)
+            if client.workspace_folders then
+              local path = client.workspace_folders[1].name
+              if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+                return
+              end
+            end
 
-      local servers = vim.list_extend(mason_servers, global_servers)
-      local lspconfig = require('lspconfig')
-      local default_capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({ capabilities = default_capabilities })
-      end
-
-      lspconfig['basedpyright'].setup({
-        capabilities = default_capabilities,
-        settings = {
-          basedpyright = {
-            disableOrganizeImports = true,
-            analysis = {
-              ignore = { '*' },
+            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+              runtime = { version = 'LuaJIT' },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME,
+                  "${3rd}/luv/library",
+                }
+              }
+            })
+          end,
+          settings = {
+            Lua = {},
+          },
+        },
+        marksman = {},
+        terraformls = {},
+        yamlls = {},
+        -- global
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              disableOrganizeImports = true,
+              analysis = {
+                ignore = { '*' },
+              },
             },
           },
         },
-      })
-
-      -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
-      lspconfig['lua_ls'].setup({
-        capabilities = default_capabilities,
-        on_init = function(client)
-          local path = client.workspace_folders[1].name
-          if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-            return
-          end
-          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-            runtime = { version = 'LuaJIT' },
-            workspace = {
-              checkThirdParty = false,
-              library = { vim.env.VIMRUNTIME },
-            },
-          })
-        end,
-        settings = { Lua = {} },
-      })
+        ruff = {},
+        tflint = {},
+      },
+    },
+    config = function(_, opts)
+      local lspconfig = require('lspconfig')
+      for server, config in pairs(opts.servers) do
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
     end,
   },
 }
