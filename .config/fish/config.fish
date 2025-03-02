673c4f5d7b1f5eaa29fe1aa0cfad1b4697d0306c
Volodymyr Chernetskyi <19735328+chernetskyi@users.noreply.github.com>
2025-03-02 21:55:22 +0100

    Revert abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,27 @@
 if status is-interactive
-    bind \cy accept-autosuggestion
+    bind \cy        accept-autosuggestion
     bind \e\[45\;7u y repaint
 
-    abbr --add a aws
-    abbr --add d docker
-    abbr --add dc docker compose
+    abbr --add a   aws
+    abbr --add d   docker
+    abbr --add dc  docker compose
     abbr --add dot dotfiles
-    abbr --add g git
-    abbr --add h helm
-    abbr --add k kubectl
-    abbr --add p python3
-    abbr --add tf terraform
+    abbr --add g   git
+    abbr --add h   helm
+    abbr --add k   kubectl
+    abbr --add kd  kubectl describe
+    abbr --add kg  kubectl get
+    abbr --add kl  kubectl logs
+    abbr --add kns kubectl config set-context --current --namespace
+    abbr --add kp  get pods
+    abbr --add p   python3
+    abbr --add tf  terraform
+    abbr --add tfa terraform apply
     abbr --add tfd terraform-docs .
+    abbr --add tff terraform fmt
+    abbr --add tfi terraform init
     abbr --add tfl tflint
-    abbr --add tg terragrunt
-    abbr --add v nvim
-
-    abbr --add --command kubectl d describe
-    abbr --add --command kubectl g get
-    abbr --add --command kubectl l logs
-    abbr --add --command kubectl ns config set-context --current --namespace
-    abbr --add --command kubectl p get pods
-
-    abbr --add --command terraform a apply
-    abbr --add --command terraform f fmt
-    abbr --add --command terraform i init
-    abbr --add --command terraform p plan
+    abbr --add tfp terraform plan
+    abbr --add tg  terragrunt
+    abbr --add v   nvim
 end
